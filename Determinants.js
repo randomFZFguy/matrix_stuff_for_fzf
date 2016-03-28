6a691b68a039a4a1fdf47ceb19a1f4f7e73015cc
randomFZFguy <r93@abv.bg>
2016-03-28 19:35:39 +0300

    Update Determinants.js

--- Determinants.js
+++ Determinants.js
@@ -1,19 +1,16 @@
 		var myDiv = $("#myDiv")[0];
 		
-		var m = {
-			reset: function rand() {
-				m.elems = [];
-				m.size = 0;
-				m.usefulRows = 0;
-				m.col = -1;
-				m.row = 0;
-			},
-			elems: [],
-			size: 0,
-			usefulRows: 0,
-			col: -1,
-			row: 0
-		};
+		var m = Object.create(null);
+		m.reset = function() {
+				this.elems = [];
+				this.size = 0;
+				this.usefulRows = 0;
+				this.col = -1;
+				this.row = 0;
+		}
+		
+		var mat = [];
+		mat[0] = Object.create(m);
 		
 		//====== GetMatrixSize ========================
 		function GetMatrixSize(form) {
@@ -27,11 +24,11 @@
 			if (size > 0 && size < 11) {
 				Explain("Matrix size: " + size);
 				Explain("Fill in the matrix. You can leave 0 cells empty.")
-				m.reset();
+				mat[0].reset();
 				AddInputTableAndHandleInputData(size);
 			} else {
 				Explain("We'll use the example matrix.", "h4")
-				m.reset();
+				mat[0].reset();
 				SetMatrix();
 			}
 		}
@@ -79,12 +76,12 @@
 			
 			function PopulateMatrix() {
 				for (var row = 0; row < size; row ++) {
-					m.elems[row] = [];
+					mat[0].elems[row] = [];
 					for (var col = 0; col < size; col ++) {
 						if (inputCell[row][col].value != "") {
-							m.elems[row][col] = inputCell[row][col].value;
+							mat[0].elems[row][col] = inputCell[row][col].value;
 						} else {
-							m.elems[row][col] = 0;
+							mat[0].elems[row][col] = 0;
 						}
 					}
 				}
@@ -96,19 +93,19 @@
 		//====== SetMatrix ============================
 		function SetMatrix(size) {
 			if (size) {
-				m.usefulRows = size;
-				m.size = size;
+				mat[0].usefulRows = size;
+				mat[0].size = size;
 				AddTable();
 				ChooseAMethod();
 			} else {
-				m.elems = [[2,4,-4,0,2,1],
+				mat[0].elems = [[2,4,-4,0,2,1],
 									[0,4,-3,0,6,0],
 									[0,2,5,13,-3.5,0],
 									[0,-2,-1,4,-0.5,-9],
 									[1,5,0,1,-5,1],
 									[1,0,2,0,-2,-2]];
-				m.size = m.elems.length;
-				m.usefulRows = m.elems.length;
+				mat[0].size = mat[0].elems.length;
+				mat[0].usefulRows = mat[0].elems.length;
 				AddTable();
 				ChooseAMethod();
 			}
@@ -134,27 +131,27 @@
 			// columns
 			var tr = document.createElement("tr");
 			tableBody.appendChild(tr);
-			for (var col = 0; col < m.size; col ++) {
+			for (var col = 0; col < mat[0].size; col ++) {
 				var th = document.createElement("th");
 				th.appendChild(document.createTextNode(col));
-				if (col == m.col) {
+				if (col == mat[0].col) {
 					th.style.color = "FF0000";
 				}
 				tr.appendChild(th);
 			}
 			
 			// rows
-			for (var row = 0; row < m.size; row ++) {
+			for (var row = 0; row < mat[0].size; row ++) {
 				var tr = document.createElement("tr");
-				for (var col = 0; col < m.size; col ++) {
+				for (var col = 0; col < mat[0].size; col ++) {
 					var td = document.createElement("td");
 					if (col == row) {
 						td.style.background = "#E1E1E1";
 					}
-					td.appendChild(document.createTextNode(m.elems[row][col]));
+					td.appendChild(document.createTextNode(mat[0].elems[row][col]));
 					tr.appendChild(td)
 					
-					if (m.col == col && (currentRow1 == row || currentRow2 == row)) {
+					if (mat[0].col == col && (currentRow1 == row || currentRow2 == row)) {
 						td.style.color = "FF0000";
 						td.style.fontWeight = "bold";
 					}
@@ -173,11 +170,11 @@
 		//====== GaussMethod ==========================
 		function GaussMethod() {
 			Explain("Gauss Elimination Method:", "h4");
-			for (var col = 0; col < m.size ; col ++) {
-				m.col ++;
-				m.row = m.col;
-				for (var row = col + 1; row < m.usefulRows; row ++) {
-					m.row ++;
+			for (var col = 0; col < mat[0].size ; col ++) {
+				mat[0].col ++;
+				mat[0].row = mat[0].col;
+				for (var row = col + 1; row < mat[0].usefulRows; row ++) {
+					mat[0].row ++;
 					HandleMatrixCell();
 				}
 				//Normalize();
@@ -187,37 +184,37 @@
 		
 		//====== HandleMatrixCell =====================
 		function HandleMatrixCell() {
-			if (m.elems[m.row][m.col] !== 0) {
-				addendRow = m.col;
+			if (mat[0].elems[mat[0].row][mat[0].col] !== 0) {
+				addendRow = mat[0].col;
 				PutZeroRowsAtTop();
 				IfDiagonalCellIsZero();
-				if (!IsRowZero() && m.elems[m.row][m.col] !== 0) {
+				if (!IsRowZero() && mat[0].elems[mat[0].row][mat[0].col] !== 0) {
 					//Normalize();
-					AddRows(addendRow, m.row, m.elems[m.row][m.col] / m.elems[addendRow][m.col]);
+					AddRows(addendRow, mat[0].row, mat[0].elems[mat[0].row][mat[0].col] / mat[0].elems[addendRow][mat[0].col]);
 				}
 			}
 		}
 			
 		//====== PutZeroRowsAtTop =====================
 		function PutZeroRowsAtTop() {
-			while (IsRowZero() && m.usefulRows > 0) {
-				m.usefulRows --;
-				SwitchRows(m.col, m.usefulRows);
+			while (IsRowZero() && mat[0].usefulRows > 0) {
+				mat[0].usefulRows --;
+				SwitchRows(mat[0].col, mat[0].usefulRows);
 			}
 		}
 		
 		//====== IfDiagonalCellIsZero =================
 		function IfDiagonalCellIsZero() {
-			if (m.elems[m.col][m.col] == 0) {
-				SwitchRows(m.row, m.col);
+			if (mat[0].elems[mat[0].col][mat[0].col] == 0) {
+				SwitchRows(mat[0].row, mat[0].col);
 			}
 		}
 	
 		//====== IsRowZero ============================
 		function IsRowZero() {
 			var rowIsZero = true;
-			for (var col = 0; col < m.size; col ++) {
-				if (m.elems[m.col][col] !== 0) {
+			for (var col = 0; col < mat[0].size; col ++) {
+				if (mat[0].elems[mat[0].col][col] !== 0) {
 					rowIsZero = false;
 					break;
 				}
@@ -227,21 +224,21 @@
 		
 		//====== Normalize ============================
 		function Normalize() {
-			var multiplier = m.elems[m.col][m.col];
+			var multiplier = mat[0].elems[mat[0].col][mat[0].col];
 			if (multiplier != 0 && multiplier != 1) {
-				for (var col = 0; col < m.size; col ++) {
-					m.elems[m.col][col] /= multiplier;
+				for (var col = 0; col < mat[0].size; col ++) {
+					mat[0].elems[mat[0].col][col] /= multiplier;
 				}
 				
-				Explain("Normalizing row " + m.col + ":");
-				AddTable(m.col);
+				Explain("Normalizing row " + mat[0].col + ":");
+				AddTable(mat[0].col);
 			}
 		}
 		
 		//====== AddRows ==============================
 		function AddRows(addend, target, multiplier) {
-			for (var col = 0; col < m.size; col ++) {
-				m.elems[target][col] -= multiplier * m.elems[addend][col];
+			for (var col = 0; col < mat[0].size; col ++) {
+				mat[0].elems[target][col] -= multiplier * mat[0].elems[addend][col];
 			}
 			
 			Explain("(row " + target + ") += (row " + addend + ") * (" + -multiplier + "):");
@@ -251,10 +248,10 @@
 		//====== SwitchRows ===========================
 		function SwitchRows(row1, row2) {
 			var temp = 0;
-			for (var col = 0; col < m.size; col ++) {
-				temp = m.elems[row1][col];
-				m.elems[row1][col] = m.elems[row2][col];
-				m.elems[row2][col] = temp;
+			for (var col = 0; col < mat[0].size; col ++) {
+				temp = mat[0].elems[row1][col];
+				mat[0].elems[row1][col] = mat[0].elems[row2][col];
+				mat[0].elems[row2][col] = temp;
 			}
 			
 			Explain("Switching row " + row1 + " with row " + row2 + ":");
@@ -264,8 +261,8 @@
 		//====== MultiplyDiagonal =====================
 		function MultiplyDiagonal() {
 			var product = 1;
-			for (var i = 0; i < m.size; i ++) {
-				product *= m.elems[i][i];
+			for (var i = 0; i < mat[0].size; i ++) {
+				product *= mat[0].elems[i][i];
 			}
 			return product;
 		}
@@ -274,101 +271,98 @@
 		//====== LaplaceNethod ========================
 		function LaplaceMethod() {
 			Explain("Laplace Method:", "h4");
-			Explain("The determinant is " + FindDeterminant(), "h4");
+			Explain("The determinant is " + FindDeterminant(mat[0].size), "h4");
 		}
 		
 		//====== FindDeterminant ======================
-		function FindDeterminant(matrix, size) {
-			if (!matrix) {
-				matrix = m.elems;
-				size = m.size;
-			} else {
-				matrix = JSON.parse(matrix);
-			}
-			
+		function FindDeterminant(size) {
 			var determinant = 0;
+			var fuck = mat[0].size - size;
 			
 			if (size > 3) {
 				for (var i = 0; i < size; i ++) {
-					// Math.pow(-1, i) = Math.pow(-1, (0 + 1) + (i + 1))
-					determinant += matrix[0][i] * Math.pow(-1, i) * FindDeterminant(GetMinor(JSON.stringify(matrix), size, 0, i), size - 1);
+					// Math.pow(-1, i) == Math.pow(-1, (0 + 1) + (i + 1))
+					GetMinor(size, 0, i);
+					determinant += mat[fuck].elems[0][i] * Math.pow(-1, i) * FindDeterminant(size - 1);
 				}
 			} else if (size == 3) {
-				determinant += Find3x3Determinant(JSON.stringify(matrix));
-				Explain("The determinant of this 3x3 matrix is " + determinant);
+				determinant += Find3x3Determinant();
+				Explain("The determinant of this 3x3 matrix is " + determinant + ".");
 			} else if (size == 2) {
-				determinant += Find2x2Determinant(JSON.stringify(matrix));
-				Explain("The determinant of this 2x2 matrix is " + determinant);
+				determinant += Find2x2Determinant();
+				Explain("The determinant of this 2x2 matrix is " + determinant + ".");
 			} else if (size == 1) {
-				determinant += Find1x1Determinant(JSON.stringify(matrix));
-				Explain("The determinant of this 1x1 matrix is " + determinant);
+				determinant += Find1x1Determinant();
+				Explain("The determinant of this 1x1 matrix is " + determinant + ".");
 			}
 			
 			return determinant;
 		}
 		
 		//====== GetMinor =============================
-		function GetMinor(matrix, size, i, j) {
-			var matrix = JSON.parse(matrix);
-			var minor = [];
-			for (var row = 0; row < size - 1; row ++) {
-				minor[row] = [];
-				for (var col = 0; col < size - 1; col ++) {
+		function GetMinor(size, i, j) {
+			var fuck = mat[0].size - size + 1;
+			mat[fuck] = Object.create(m);
+			mat[fuck].reset();
+			mat[fuck].size = size - 1;
+			
+			for (var row = 0; row < mat[fuck].size; row ++) {
+				mat[fuck].elems[row] = [];
+				for (var col = 0; col < mat[fuck].size; col ++) {
 					if (col >= j) {
 						if (row >= i) {
-							minor[row][col] = matrix[row + 1][col + 1];
+							mat[fuck].elems[row][col] = mat[fuck - 1].elems[row + 1][col + 1];
 						} else {
-							minor[row][col] = matrix[row + 1][col + 1];
+							mat[fuck].elems[row][col] = mat[fuck - 1].elems[row + 1][col + 1];
 						}
 					} else {
 						if (row >= i) {
-							minor[row][col] = matrix[row + 1][col];
+							mat[fuck].elems[row][col] = mat[fuck - 1].elems[row + 1][col];
 						} else {
-							minor[row][col] = matrix[row][col];
+							mat[fuck].elems[row][col] = mat[fuck - 1].elems[row][col];
 						}
 					}
 				}
 			}
 			Explain("The minor with row " + i + " and column " + j + " removed:");
-			AddTableLaplace(JSON.stringify(minor), size - 1);
-			return JSON.stringify(minor);
+			AddTableLaplace(mat[fuck].size);
 		}
 		
 		//====== Find3x3Determinant ===================
-		function Find3x3Determinant(matrix) {
-			var matrix = JSON.parse(matrix);
-			var determinant = matrix[0][0] * matrix[1][1] * matrix[2][2] +
-												matrix[0][1] * matrix[1][2] * matrix[2][0] +
-												matrix[0][2] * matrix[1][0] * matrix[2][1] -
-												matrix[0][0] * matrix[1][2] * matrix[2][1] -
-												matrix[0][1] * matrix[1][0] * matrix[2][2] -
-												matrix[0][2] * matrix[1][1] * matrix[2][0];
+		function Find3x3Determinant() {
+			var fuck = mat[0].size - 3;
+			var determinant = mat[fuck].elems[0][0] * mat[fuck].elems[1][1] * mat[fuck].elems[2][2] +
+												mat[fuck].elems[0][1] * mat[fuck].elems[1][2] * mat[fuck].elems[2][0] +
+												mat[fuck].elems[0][2] * mat[fuck].elems[1][0] * mat[fuck].elems[2][1] -
+												mat[fuck].elems[0][0] * mat[fuck].elems[1][2] * mat[fuck].elems[2][1] -
+												mat[fuck].elems[0][1] * mat[fuck].elems[1][0] * mat[fuck].elems[2][2] -
+												mat[fuck].elems[0][2] * mat[fuck].elems[1][1] * mat[fuck].elems[2][0];
 			return determinant;
 		}
 		
 		//====== Find2x2Determinant ===================
-		function Find2x2Determinant(matrix) {
-			var matrix = JSON.parse(matrix);
-			var determinant = matrix[0][0] * matrix[1][1] -
-												matrix[0][1] * matrix[1][0];
+		function Find2x2Determinant() {
+			var fuck = mat[0].size - 2;
+			var determinant = mat[fuck].elems[0][0] * mat[fuck].elems[1][1] -
+												mat[fuck].elems[0][1] * mat[fuck].elems[1][0];
 			return determinant;
 		}
 		
 		//====== Find1x1Determinant ===================
-		function Find1x1Determinant(matrix) {
-			var matrix = JSON.parse(matrix);
-			var determinant = matrix[0][0] * 1; // to make it a number
+		function Find1x1Determinant() {
+			var fuck = mat[0].size - 1;
+			var determinant = mat[fuck].elems[0][0] * 1; // to make it a number
 			return determinant;
 		}
 		
 		//====== AddTableLaplace ======================
-		function AddTableLaplace(matrix, size) {
+		function AddTableLaplace(size) {
+			var fuck = mat[0].size - size;
+			
 			var table = document.createElement("table");
-			table.className += " table table-bordered";
+			table.className += "table table-bordered";
 			var tableBody = document.createElement("tbody");
 			table.appendChild(tableBody);
-			
-			var matrix = JSON.parse(matrix);
 			
 			// columns
 			var tr = document.createElement("tr");
@@ -387,7 +381,7 @@
 					if (col == row) {
 						td.style.background = "#E1E1E1";
 					}
-					td.appendChild(document.createTextNode(matrix[row][col]));
+					td.appendChild(document.createTextNode(mat[fuck].elems[row][col]));
 					tr.appendChild(td)
 				}
 				
